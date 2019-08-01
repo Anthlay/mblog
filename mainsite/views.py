121c847f5e6a928ce50de9a84657bcea7180fbf8
assue <455775371@qq.com>
2019-08-01 20:59:24 +0800

    19.8.1.20.59

---
+++ mainsite/views.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Post
+# Create your views here
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count,post in enumerate(posts):
+        posts = Post.objects.all()
+        now = datetime.now()
+    return render(request,'index.html',locals())
+        #post_lists.append("No.{}:".format(str(count))+str(post)+"<br>")
+        #post_lists.append("<small>"+str(post.body)+"</small><br><br>")
+    #return HttpResponse(post_lists)
+def showpost(request,slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            return render(request,'post.html',locals())
+    except:
+        return redirect('/')
